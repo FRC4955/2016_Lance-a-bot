0e1f2af4352ddfe9c46efba7c8cdc2031f1e9ec2
Cvarier <cvvarier@gmail.com>
2016-02-20 21:33:50 -0500

    Update IntakeRollers.java

--- src/org/usfirst/frc/team4955/robot/IntakeRollers.java
+++ src/org/usfirst/frc/team4955/robot/IntakeRollers.java
@@ -7,19 +7,19 @@ public class IntakeRollers {
 	
 	private Talon left, right;
 	
-	IntakeRollers(int leftPWM, int rightPWM) {
+	IntakeRollers (int leftPWM, int rightPWM) {
     	left = new Talon(leftPWM);
     	right = new Talon(rightPWM);
 	}
 	
 	void run(Joystick controlStick) {
 		
-	        if(controlStick.getRawButton(Constants.EIGHT_BUTTON_ATTACK3)) {
+	        if (controlStick.getRawButton(Constants.EIGHT_BUTTON_ATTACK3)) {
 	        	System.out.println("Intake out (Button 8)");
 	        	left.set(1.0);
 	        	right.set(-1.0);
 	        	
-	        } else if(controlStick.getRawButton(Constants.SEVEN_BUTTON_ATTACK3)) {
+	        } else if (controlStick.getRawButton(Constants.SEVEN_BUTTON_ATTACK3)) {
 	        	System.out.println("Intake in (Button 7)");
 	        	left.set(-1.0);
 	        	right.set(1.0);
